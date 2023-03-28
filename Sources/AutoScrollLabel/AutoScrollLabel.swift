391da60c485422758e84bd551db60681cf22b9e4
HShotaro <shotaro.10965@gmail.com>
2023-03-28 11:25:24 +0900

    initial commit

---
+++ Sources/AutoScrollLabel/AutoScrollLabel.swift
@@ -0,0 +1,6 @@
+public struct AutoScrollLabel {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/AutoScrollLabelTests/AutoScrollLabelTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AutoScrollLabel
+
+final class AutoScrollLabelTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AutoScrollLabel().text, "Hello, World!")
+    }
+}
